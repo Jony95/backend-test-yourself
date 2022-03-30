d872087fff1bc5ab0e46cc35db53c585493d1ef7
Vladimir Chim <vladimir.chim@amdaris.com>
2022-03-30 19:12:58 +0300

    first commit

---
+++ CodingTest/CodingTest/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace CodingTest
+{
+    public class Program  
+    {
+        public static List<B> Map<A, B>(List<A> list, Func<A, B> f)
+        {
+            var result = new List<B>();
+
+            foreach (var a in list)
+            {
+                result.Add(f(a));
+            }
+
+            return result;
+        }
+
+        public static B Fold<A, B>(List<A> list, B initial, Func<B, A, B> folder)
+        {
+            foreach (var a in list)
+            {
+                initial = folder(initial, a);
+            }
+
+            return initial;
+        }
+
+        public static List<B> Map2<A, B>(List<A> list, Func<A, B> f)
+        {
+            var k = Fold(list, new List<B>(), (arr, x) =>
+            {
+                arr.Add(f(x));
+                return arr;
+            });
+
+            return k;
+        }
+
+        public static void Main(string[] args)
+        {
+            var a = new List<int>() {1,2,3};
+
+            var b = Map2(a, x => x.ToString() + '%');
+
+            foreach (var i in b)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
